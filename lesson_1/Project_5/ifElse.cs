a8b03cb223ccb3746f20f46ea2e65b5989ae206f
Svetlana Lvovskaya <svetlana.lvovskaya@gmail.com>
2022-10-22 20:00:40 +0500

    add Project_5

---
+++ lesson_1/Project_5/ifElse.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "cветик")
+{
+    Console.WriteLine("Ура, это же СВЕТИК!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
